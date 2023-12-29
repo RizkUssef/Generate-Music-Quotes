4e328fcbe93ea9169bb5b279dd9f9f4178f9f061
Rizk Ussef <rizkusgt67@gmail.com>
2023-12-29 18:25:57 +0200

    push the project

---
+++ js/next.js
@@ -0,0 +1,219 @@
+let song_name = document.getElementById("song_name");
+let singer_name = document.getElementById("singer_name");
+let singer_img = document.getElementById("singer_img");
+let qoute = document.getElementById("qoute");
+let lo = document.getElementById("lo");
+let next_btn = document.getElementById("next_btn");
+
+const qoutes = [
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-1.jpeg`,
+    song_name: `ana fe entezarak`,
+    singer_name: `umm kulthum`,
+    qoute: `يا ريتني.. يا ريتني عمري ما حبيت<br>
+        توعدني بسنين وأيَّام، وتجيني بحجج وكلام<br>
+        وتسلِّم وتمرِّ قوام او تخلف وتقول لي نسيت<br>
+        يا ريت يا ريت`,
+    logo: `imgs/RFTU-0.svg`,
+  },
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-2.jpeg`,
+    song_name: `Ba'eed Anak`,
+    singer_name: `umm kulthum`,
+    qoute: `افتكر لي لحظة حلوة عشنا فيها للهوا
+    <br>
+    افتكر لي مرة غنوة يوم سمعناها سوا
+    <br>
+    خد من عمري عمري كله الا ثواني اشوفك فيها`,
+    logo: `imgs/RFTU-1.svg`,
+  },
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-3.jpg`,
+    song_name: `Hayart Qalby Maak`,
+    singer_name: `umm kulthum`,
+    qoute: `ياما ليالي أنا وخيالي ، أفضل أصبر روحي بكلمة يوم قلتها لي
+        وبات أفكر.. في اللي جرى لك.. واللي جرى لي`,
+    logo: `imgs/RFTU-2.svg`,
+  },
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-4.jpeg`,
+    song_name: `ansak`,
+    singer_name: `umm kulthum`,
+    qoute: `كان لك معايا اجمل حكاية فى العمر كله
+        <br>
+        سنين بحالها ..ما فات جمالها..على حب قبله
+        <br>
+        سنين ومرت زى الثوانى فى حبك انت
+        <br>
+        وإن كنت أقدر أحب تانى أحبك انت`,
+    logo: `imgs/RFTU-3.svg`,
+  },
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-5.jpeg`,
+    song_name: `El Hob Keda`,
+    singer_name: `umm kulthum`,
+    qoute: `حبيبي لما يوعدني تبات الدنيا ضحكالي
+    <br>
+    ولما وصله يسعدني ما افكر فى اللى يجرالي
+    <br>
+    ينسيني الوجود كله ولا يخطر على بالي`,
+    logo: `imgs/RFTU-4.svg`,
+  },
+  {
+    singer_img: `imgs/Umm-Kulthum/Umm-Kulthum-5.jpeg`,
+    song_name: `El Hob Keda`,
+    singer_name: `umm kulthum`,
+    qoute: `وبعد الليل يجينا النور وبعد الغيم ربيع وزهور
+    <br>
+    اهو من ده وده الحب كده مش عايزه كلام`,
+    logo: `imgs/RFTU-4.svg`,
+  },
+//   hoda
+  {
+    singer_img: `imgs/Mohammed Abdel Wahab/Mohammed Abdel Wahab-1.jpeg`,
+    song_name: `Ay Ser Fek`,
+    singer_name: `Mohammed Abdel Wahab`,
+    qoute: `أَيُ سِرٍ فِيكِ إِنِّي لَسْتُ أَدْرِي
+    <br>
+    كُلُّ مَا فِيكِ مِنَ الأَسْرَارِ يُغْرِي`,
+    logo: `imgs/RFTU-1.svg`,
+  },
+  {
+    singer_img: `imgs/Mohammed Abdel Wahab/Mohammed Abdel Wahab-2.jpeg`,
+    song_name: `Ana We El Azab We Hawak`,
+    singer_name: `Mohammed Abdel Wahab`,
+    qoute:`أنا والعذاب وهواك .. عايشين لبعضينا
+    <br>
+    اخرتها ايه وياك .. يا اللى أنت ناسينا`,
+    logo: `imgs/RFTU-2.svg`,
+  },
+  {
+    singer_img: `imgs/Mohammed Abdel Wahab/Mohammed Abdel Wahab-3.jpeg`,
+    song_name: `Men Gheir Leh`,
+    singer_name: `Mohammed Abdel Wahab`,
+    qoute: `بحلم وأصحى وأعيش على حبك
+    <br>
+    حتى في عز عذابي بحبك
+    <br>
+    عارف ليه . من غير ليه
+    <br>
+    من غير ليه . آه يا حبيبي بحبك`,
+    logo: `imgs/RFTU-3.svg`,
+  },
+  {
+    singer_img: `imgs/Mohammed Abdel Wahab/Mohammed Abdel Wahab-4.jpeg`,
+    song_name: `Khay Khay`,
+    singer_name: `Mohammed Abdel Wahab`,
+    qoute: `و فات معايا كلام م الحلو و الغالي
+    <br>
+    يهز قلب الحجر لو يفهمه الخالي`,
+    logo: `imgs/RFTU-4.svg`,
+  },
+// holam
+  {
+    singer_img: `imgs/Adbul Haleem Hafez/Adbul Haleem Hafez-1.jpeg`,
+    song_name: `Zalamoh`,
+    singer_name: `Abdel Haleem Hafez`,
+    qoute: `جبت الطبيب يداوي سألني الجرح فيـن
+    <br>
+    قلت اسأل دق قلبي اللى زايد دقتيـن
+    <br>
+    سمع فى القلب حاجه وقال ده رمش عين
+    <br>
+    صاحبه رماه وناسي بقاله جمعتين`,
+    logo: `imgs/RFTU-1.svg`,
+  },
+  {
+    singer_img: `imgs/Adbul Haleem Hafez/Adbul Haleem Hafez-2.jpeg`,
+    song_name: `Sawwah`,
+    singer_name: `Abdel Haleem Hafez`,
+    qoute:`لا أنا عارف أرتاح
+
+    وأنا تايه سواح
+    <br>
+    يا قمر يا ناسيني رسيني عاللي غايب
+    <br>
+    نور لي .. وريني سكة الحبايب`,
+    logo: `imgs/RFTU-0.svg`,
+  },
+  {
+    singer_img: `imgs/Adbul Haleem Hafez/Adbul Haleem Hafez-3.jpeg`,
+    song_name: `Zay Elhawa`,
+    singer_name: `Abdel Haleem Hafez`,
+    qoute: `و اتارينى ماسك الهوا بإيديا ماسك الهوا
+    <br>
+    و آه من الهوا يا حبيبى آه من الهوا`,
+    logo: `imgs/RFTU-3.svg`,
+  },
+  {
+    singer_img: `imgs/Adbul Haleem Hafez/Adbul Haleem Hafez-4.jpeg`,
+    song_name: `Awel Marah Teheb`,
+    singer_name: `Abdel Haleem Hafez`,
+    qoute: `ليه بيقولو الحب أسيّه .. ليه بيقولو شجن ودموع ؟
+    <br>
+    اول حب يمر عليّا .. قاد لي الدنيا فرح وشموع`,
+    logo: `imgs/RFTU-5.svg`,
+  },
+// sbah
+  {
+    singer_img: `imgs/Sabah Fakhri/Sabah Fakhri-1.jpg`,
+    song_name: `ana fy sokrayn`,
+    singer_name: `sabah fakhri`,
+    qoute: `أنا في سكرين من خمر وعين
+    <br>
+    واحتراق في لهيب الشفتين
+    <br>
+    لا تزدني بل فزدني
+    `,
+    logo: `imgs/RFTU-0.svg`,
+  },
+  {
+    singer_img: `imgs/Sabah Fakhri/Sabah Fakhri-2.jpeg`,
+    song_name: `Malek ya hilwa`,
+    singer_name: `Sabah Fakhri`,
+    qoute:`مالك يا حلوة مالك
+    هو الغرام غير حالك
+    <br>
+    فتنوا عليكي عذالك لما رؤوكي بجمالك
+    `,
+    logo: `imgs/RFTU-0.svg`,
+  },
+  {
+    singer_img: `imgs/Sabah Fakhri/Sabah Fakhri-3.jpg`,
+    song_name: `Ana Wa Habibi`,
+    singer_name: `Sabah Fakhri`,
+    qoute: `أنا و حبيبي في جنينة و الورد مخيم علينا
+    <br>
+    عيونك سود يا محلاهم قلبي تلوع بهواهم`,
+    logo: `imgs/RFTU-3.svg`,
+  },
+  {
+    singer_img: `imgs/Sabah Fakhri/Sabah Fakhri-3.jpg`,
+    song_name: `Hymatny`,
+    singer_name: `Sabah Fakhri`,
+    qoute: `هيمتني تيمتني
+    عن سواها أشغلتني
+    <br>
+    أخت شمس ذات أنس
+    دون كاس أسكرتني`,
+    logo: `imgs/RFTU-4.svg`,
+  },
+  {
+    singer_img: `imgs/Sabah Fakhri/Sabah Fakhri-1.jpg`,
+    song_name: `Ana Wa Habibi`,
+    singer_name: `Sabah Fakhri`,
+    qoute: `قلبي تلوع يا وعدي ... بدي طبيب يداويني
+    <br>
+    دوا الطبيب ما ينفعني ... شوفة حبيبي بتشفيني`,
+    logo: `imgs/RFTU-5.svg`,
+  },
+];
+
+next_btn.addEventListener("click", function () {
+  let rand = Math.floor(Math.random() * qoutes.length);
+  singer_img.src = qoutes[rand].singer_img;
+  song_name.innerText = qoutes[rand].song_name;
+  singer_name.innerText = qoutes[rand].singer_name;
+  qoute.innerHTML = qoutes[rand].qoute;
+  lo.src = qoutes[rand].logo;
+});
